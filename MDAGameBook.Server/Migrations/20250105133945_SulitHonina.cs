7d2f49f8d70dc49ed599db3bd1dd418325fe039e
st:prchal <steveruu@stvr.cz>
2025-01-05 15:37:27 +0100

    sulithonina migraton

---
+++ MDAGameBook.Server/Migrations/20250105133945_SulitHonina.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MDAGameBook.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SulitHonina : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "049f5a01-7bd4-4e14-9b0d-d1a678846d84", "650d5e9c-6590-434f-89da-32af8a3ebd5b" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "049f5a01-7bd4-4e14-9b0d-d1a678846d84");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "650d5e9c-6590-434f-89da-32af8a3ebd5b");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "6e36d565-a5a0-4d88-b258-2336c67cb176", null, "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "220ad197-8788-4706-a987-64512bd849fb", 0, "023bb6ee-1905-4ad6-91b7-55946e6465c1", "admin@minjiya.com", true, false, null, "ADMIN@MINJIYA.COM", "ADMIN@MINJIYA.COM", "AQAAAAIAAYagAAAAEBYzprsbxKl4RCYt7Cyo/YJK0s918UOOmQYTkEW6QWXtKQQ4Tl19Y3xekJbmVWkM5A==", null, false, "", false, "admin@minjiya.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "6e36d565-a5a0-4d88-b258-2336c67cb176", "220ad197-8788-4706-a987-64512bd849fb" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "6e36d565-a5a0-4d88-b258-2336c67cb176", "220ad197-8788-4706-a987-64512bd849fb" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6e36d565-a5a0-4d88-b258-2336c67cb176");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "220ad197-8788-4706-a987-64512bd849fb");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "049f5a01-7bd4-4e14-9b0d-d1a678846d84", null, "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "650d5e9c-6590-434f-89da-32af8a3ebd5b", 0, "d437938c-aa70-4ab3-836c-21abb499ff13", "admin@minjiya.com", true, false, null, "ADMIN@MINJIYA.COM", "ADMIN@MINJIYA.COM", "AQAAAAIAAYagAAAAEEkabM8dIDbmXiBl336LargHm+uR1+7oV602Nm0b4Jv4lFFUhe634kipk2GBwGKkbA==", null, false, "", false, "admin@minjiya.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "049f5a01-7bd4-4e14-9b0d-d1a678846d84", "650d5e9c-6590-434f-89da-32af8a3ebd5b" });
+        }
+    }
+}
